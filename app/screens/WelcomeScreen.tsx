953f36a29730d2025a6c09f63a3214e76ab9910f
Suzy Kenne <suzyndatewo@gmail.com>
2025-02-05 17:14:23 +0100

    designed welcome scren

---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,50 @@
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import colors from '../config/colors';
+
+export default function WelcomeScreen() {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require('@/assets/images/background.jpg')}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require('@/assets/images/logo-red.png')}
+        />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: colors.primary,
+  },
+  registerButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: colors.secondary,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
+  },
+});

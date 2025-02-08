e86ee6bfd88825ce42ec4021b7c2c2d7d90f4874
Suzy Kenne <suzyndatewo@gmail.com>
2025-02-08 13:31:54 +0100

    added delete action

---
+++ app/components/ListItemDelteAction.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import colors from '../config/colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+
+interface Props {
+  onPress?: () => void;
+}
+export default function ListItemDelteAction({ onPress }: Props) {
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <MaterialCommunityIcons
+          name="trash-can"
+          size={30}
+          color={colors.white}
+        />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.danger,
+    width: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

40b42d9761bc08329a5264845709b299a46e6b94
Suzy Kenne <suzyndatewo@gmail.com>
2025-02-12 15:36:21 +0100

    worked on the index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, SafeAreaView, Platform, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Platform,
+  View,
+  TextInput,
+} from 'react-native';
 import WelcomeScreen from '../screens/WelcomeScreen';
 // import ViewImage from '../screens/ViewImage';
 
@@ -10,17 +17,31 @@ import MesagesScreen from '../screens/MesagesScreen';
 import Screen from '../components/Screen';
 import Icon from '../components/Icon';
 import ListItem from '../components/ListItem';
+import AccountScreen from '../screens/AccountScreen';
+import ListingsScreen from '../screens/ListingsScreen';
+import styles from '../components/AppText/style';
+import { useState } from 'react';
 
 export default function App() {
+  const [firstName, setFirstName] = useState('');
   return (
     <Screen>
-      <ListItem
-        title="My title"
-        subtitle="my subtitle"
-        ImageComponent={<Icon name="email" size={30} backgroundColor="white" />}
+      <Text>{firstName}</Text>
+      <TextInput
+        clearButtonMode="always"
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="first name"
+        style={{ borderBottomColor: '#ccc', borderBottomWidth: 1 }}
       />
-      {/* <Icon name="email" size={50} backgroundColor="red" iconColor="white" /> */}
     </Screen>
+    // <Screen>
+    //   <ListItem
+    //     title="My title"
+    //     subtitle="my subtitle"
+    //     ImageComponent={<Icon name="email" size={30} backgroundColor="white" />}
+    //   />
+    //   {/* <Icon name="email" size={50} backgroundColor="red" iconColor="white" /> */}
+    // <ListingsScreen />
   );
   // return <ViewImage />;
   // return (

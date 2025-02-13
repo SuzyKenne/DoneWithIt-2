df59d94f48668574fadd8475cd13697444576d34
Suzy Kenne <suzyndatewo@gmail.com>
2025-02-13 13:09:17 +0100

    added app picker component

---
+++ app/components/AppPicker.tsx
@@ -0,0 +1,85 @@
+import {
+  Button,
+  FlatList,
+  Modal,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import colors from '../config/colors';
+import defaultStyle from '../config/style';
+import AppText from './AppText/AppText';
+import Screen from './Screen';
+import { TouchableWithoutFeedback } from 'react-native';
+import PickerItem from './PickerItem';
+
+interface CategoryItem {
+  label: string;
+  value: number;
+}
+
+interface Props {
+  icon?: keyof typeof MaterialCommunityIcons.glyphMap;
+  placeholder: string;
+  items: CategoryItem[];
+}
+export default function AppPicker({ icon, placeholder, items }: Props) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={colors.medium}
+              style={styles.icon}
+            />
+          )}
+
+          <AppText>{placeholder}</AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={defaultStyle.colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.value.toString()}
+            renderItem={({ item }) => (
+              <PickerItem
+                label={item.label}
+                onPress={() => console.log('tapped')}
+              />
+            )}
+          />
+        </Screen>
+      </Modal>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: 'row',
+    width: '100%',
+    padding: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+});

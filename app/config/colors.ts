4086806c87ac09baaeb54edc43c9cb503e53e90d
Suzy Kenne <suzyndatewo@gmail.com>
2025-02-13 13:11:27 +0100

    updated color config

--- app/config/colors.ts
+++ app/config/colors.ts
@@ -1,3 +1,5 @@
+import { DarkTheme } from '@react-navigation/native';
+
 export default {
   primary: '#fc5c65',
   secondary: '#4ecdc4',
@@ -6,4 +8,5 @@ export default {
   medium: '#6e6969',
   light: '#f8f4f4',
   danger: '#ff5252',
+  DarkTheme: '#0c0c0c',
 };
